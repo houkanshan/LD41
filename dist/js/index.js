641e81efd3cbf0a24c53ba1c43acc2bf6ebc7ff8
houkanshan <houkanshan@gmail.com>
2018-04-24 20:30:05 +0800

    improve marquee

--- dist/js/index.js
+++ dist/js/index.js
@@ -10588,18 +10588,43 @@ var Textarea = (function () {
 
 "use strict";
 /* harmony export (immutable) */ __webpack_exports__["a"] = renderMarquee;
+(function () {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+            || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function (callback) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () { callback(currTime + timeToCall); }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+}());
 function renderMarquee(el) {
     var width = el.outerWidth(true);
     var _el = el.clone();
     el.parent().append(_el);
     var currLeft = 0;
     var els = el.parent();
-    setTimeout(function next() {
-        els.css('transform', "translate3d(" + -currLeft + "px, 0, 0)");
-        currLeft += 1;
-        currLeft %= width;
-        setTimeout(next, 20);
-    }, 20);
+    var lastTime = null;
+    requestAnimationFrame(function next(time) {
+        if (lastTime) {
+            currLeft += (time - lastTime) / 20;
+            currLeft %= width;
+            els.css('transform', "translate3d(" + -currLeft + "px, 0, 0)");
+        }
+        lastTime = time;
+        requestAnimationFrame(next);
+    });
 }
 
 
--- js/marquee.ts
+++ js/marquee.ts
@@ -1,13 +1,41 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
 export function renderMarquee(el) {
   const width = el.outerWidth(true)
   const _el = el.clone()
   el.parent().append(_el)
   let currLeft = 0
   const els = el.parent()
-  setTimeout(function next() {
-    els.css('transform', `translate3d(${-currLeft}px, 0, 0)`)
-    currLeft += 1
-    currLeft %= width
-    setTimeout(next, 20)
-  }, 20)
+  let lastTime = null
+  requestAnimationFrame(function next(time) {
+    if (lastTime) {
+      currLeft += (time - lastTime) / 20
+      currLeft %= width
+      els.css('transform', `translate3d(${-currLeft}px, 0, 0)`)
+    }
+    lastTime = time
+    requestAnimationFrame(next)
+  })
 }
